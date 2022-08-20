e4951c1b01cecc0135d4ada50dd0f485b2136a6a
akarshhemrajanime <83828630+akarshhemrajanime@users.noreply.github.com>
2022-08-20 10:54:38 +0700

    Add files via upload

---
+++ sosialad.js
@@ -0,0 +1,162 @@
+function js_thml(){
+  let code = "";
+  code += '<!-- Atur posisi sesuai keinginan dengan cara memberikan class (atas, tengah, atau bawah) seperti contoh di bawah default atas -->\n';
+  code += '<div class=\'gila_temax_looping atas\'>\n';
+  code += '  <!-- Content 1 -->\n';
+  code += '  <div class=\'looping_ad_wrap\'>\n';
+  code += '\t<div class=\'head_left\'>\n';
+  code += '\t  <div class=\'thumb\'>\n';
+  code += '\t\t<!-- Atur Gambar sesuai keinginan-->\n';
+  code += '\t\t<img alt=\'Image\' src=\'MASUKKAN_URL_GAMBAR_DISINI\'/>\n';
+  code += '\t  </div>\n';
+  code += '\t  <div class=\'description\'>\n';
+  code += '\t\t<!-- Atur Judul sesuai keinginan-->\n';
+  code += '\t\t<span>TULIS JUDUL DISINI</span>\n';
+  code += '\t\t<!-- Atur Deskripsi sesuai keinginan -->\n';
+  code += '\t\t<p>TULIS DESKRIPSI DISINI</p>\n';
+  code += '\t  </div>\n';
+  code += '\t</div>\n';
+  code += '\t<div class=\'head_right\'>\n';
+  code += '\t  <!-- Atur Link sesuai keinginan -->\n';
+  code += '\t  <a class=\'link_button\' href=\'MASUKKAN_LINK_TUJUAN_DISINI\'>\n';
+  code += '\t\tTULIS NAMA TOMBOL DISINI\n';
+  code += '\t  </a>\n';
+  code += '\t  <div class=\'close_button\'>\n';
+  code += '\t\t<svg viewBox=\'0 0 24 24\'>\n';
+  code += '\t\t  <path d=\'M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z\'/>\n';
+  code += '\t\t</svg>\n';
+  code += '\t  </div>\n';
+  code += '\t</div>\n';
+  code += '  </div>\n';
+
+  code += '  \n';
+  code += '  <!-- Content 2 -->\n';
+  code += '  <div class=\'looping_ad_wrap\'>\n';
+  code += '\t<div class=\'head_left\'>\n';
+  code += '\t  <div class=\'thumb\'>\n';
+  code += '\t\t<!-- Atur Gambar sesuai keinginan-->\n';
+  code += '\t\t<img alt=\'Image\' src=\'MASUKKAN_URL_GAMBAR_DISINI\'/>\n';
+  code += '\t  </div>\n';
+  code += '\t  <div class=\'description\'>\n';
+  code += '\t\t<!-- Atur Judul sesuai keinginan-->\n';
+  code += '\t\t<span>TULIS JUDUL DISINI</span>\n';
+  code += '\t\t<!-- Atur Deskripsi sesuai keinginan -->\n';
+  code += '\t\t<p>TULIS DESKRIPSI DISINI</p>\n';
+  code += '\t  </div>\n';
+  code += '\t</div>\n';
+  code += '\t<div class=\'head_right\'>\n';
+  code += '\t  <!-- Atur Link sesuai keinginan -->\n';
+  code += '\t  <a class=\'link_button\' href=\'MASUKKAN_LINK_TUJUAN_DISINI\'>\n';
+  code += '\t\tTULIS NAMA TOMBOL DISINI\n';
+  code += '\t  </a>\n';
+  code += '\t  <div class=\'close_button\'>\n';
+  code += '\t\t<svg viewBox=\'0 0 24 24\'>\n';
+  code += '\t\t  <path d=\'M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z\'/>\n';
+  code += '\t\t</svg>\n';
+  code += '\t  </div>\n';
+  code += '\t</div>\n';
+  code += '  </div>\n';
+
+  code += '\n';
+  code += '  <!-- Content 3 -->\n';
+  code += '  <div class=\'looping_ad_wrap\'>\n';
+  code += '\t<div class=\'head_left\'>\n';
+  code += '\t  <div class=\'thumb\'>\n';
+  code += '\t\t<!-- Atur Gambar sesuai keinginan-->\n';
+  code += '\t\t<img alt=\'Image\' src=\'MASUKKAN_URL_GAMBAR_DISINI\'/>\n';
+  code += '\t  </div>\n';
+  code += '\t  <div class=\'description\'>\n';
+  code += '\t\t<!-- Atur Judul sesuai keinginan-->\n';
+  code += '\t\t<span>TULIS JUDUL DISINI</span>\n';
+  code += '\t\t<!-- Atur Deskripsi sesuai keinginan -->\n';
+  code += '\t\t<p>TULIS DESKRIPSI DISINI</p>\n';
+  code += '\t  </div>\n';
+  code += '\t</div>\n';
+  code += '\t<div class=\'head_right\'>\n';
+  code += '\t  <!-- Atur Link sesuai keinginan -->\n';
+  code += '\t  <a class=\'link_button\' href=\'MASUKKAN_LINK_TUJUAN_DISINI\'>\n';
+  code += '\t\tTULIS NAMA TOMBOL DISINI\n';
+  code += '\t  </a>\n';
+  code += '\t  <div class=\'close_button\'>\n';
+  code += '\t\t<svg viewBox=\'0 0 24 24\'>\n';
+  code += '\t\t  <path d=\'M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z\'/>\n';
+  code += '\t\t</svg>\n';
+  code += '\t  </div>\n';
+  code += '\t</div>\n';
+  code += '  </div>\n';
+
+  code += '\n';
+  code += '  <!-- Content 4 -->\n';
+  code += '  <div class=\'looping_ad_wrap\'>\n';
+  code += '\t<div class=\'head_left\'>\n';
+  code += '\t  <div class=\'thumb\'>\n';
+  code += '\t\t<!-- Atur Gambar sesuai keinginan-->\n';
+  code += '\t\t<img alt=\'Image\' src=\'MASUKKAN_URL_GAMBAR_DISINI\'/>\n';
+  code += '\t  </div>\n';
+  code += '\t  <div class=\'description\'>\n';
+  code += '\t\t<!-- Atur Judul sesuai keinginan-->\n';
+  code += '\t\t<span>TULIS JUDUL DISINI</span>\n';
+  code += '\t\t<!-- Atur Deskripsi sesuai keinginan -->\n';
+  code += '\t\t<p>TULIS DESKRIPSI DISINI</p>\n';
+  code += '\t  </div>\n';
+  code += '\t</div>\n';
+  code += '\t<div class=\'head_right\'>\n';
+  code += '\t  <!-- Atur Link sesuai keinginan -->\n';
+  code += '\t  <a class=\'link_button\' href=\'MASUKKAN_LINK_TUJUAN_DISINI\'>\n';
+  code += '\t\tTULIS NAMA TOMBOL DISINI\n';
+  code += '\t  </a>\n';
+  code += '\t  <div class=\'close_button\'>\n';
+  code += '\t\t<svg viewBox=\'0 0 24 24\'>\n';
+  code += '\t\t  <path d=\'M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z\'/>\n';
+  code += '\t\t</svg>\n';
+  code += '\t  </div>\n';
+  code += '\t</div>\n';
+  code += '  </div>\n';
+
+  code += '\n';
+  code += '  <!-- Content 5 -->\n';
+  code += '  <div class=\'looping_ad_wrap\'>\n';
+  code += '\t<div class=\'head_left\'>\n';
+  code += '\t  <div class=\'thumb\'>\n';
+  code += '\t\t<!-- Atur Gambar sesuai keinginan-->\n';
+  code += '\t\t<img alt=\'Image\' src=\'MASUKKAN_URL_GAMBAR_DISINI\'/>\n';
+  code += '\t  </div>\n';
+  code += '\t  <div class=\'description\'>\n';
+  code += '\t\t<!-- Atur Judul sesuai keinginan-->\n';
+  code += '\t\t<span>TULIS JUDUL DISINI</span>\n';
+  code += '\t\t<!-- Atur Deskripsi sesuai keinginan -->\n';
+  code += '\t\t<p>TULIS DESKRIPSI DISINI</p>\n';
+  code += '\t  </div>\n';
+  code += '\t</div>\n';
+  code += '\t<div class=\'head_right\'>\n';
+  code += '\t  <!-- Atur Link sesuai keinginan -->\n';
+  code += '\t  <a class=\'link_button\' href=\'MASUKKAN_LINK_TUJUAN_DISINI\'>\n';
+  code += '\t\tTULIS NAMA TOMBOL DISINI\n';
+  code += '\t  </a>\n';
+  code += '\t  <div class=\'close_button\'>\n';
+  code += '\t\t<svg viewBox=\'0 0 24 24\'>\n';
+  code += '\t\t  <path d=\'M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z\'/>\n';
+  code += '\t\t</svg>\n';
+  code += '\t  </div>\n';
+  code += '\t</div>\n';
+  code += '  </div>\n';
+
+  code += '</div>\n';
+  return code;
+}
+document.getElementById("js_html").innerHTML = js_thml();
+
+$(function() {
+  var $anchors = $('.gila_temax_looping .looping_ad_wrap');
+
+  (function loop_element(idx) {
+    $anchors.removeClass('active').eq(idx).addClass('active');
+    setTimeout(function () {
+      loop_element((idx + 1) % $anchors.length);
+    }, 3000); // Atur waktu sesuai keinginan dengan format (1000 = 1 detik)
+  }(0));
+});
+
+$('.close_button').click(function(){
+  $('.looping_ad_wrap.active').remove();
+});
